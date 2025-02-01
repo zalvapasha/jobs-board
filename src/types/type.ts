ef145ac783d974be77ad3e97849f4ff34b7b88f3
zalvapasha <zalvapasha1@gmail.com>
2025-02-01 21:31:43 +0700

    first commit

---
+++ src/types/type.ts
@@ -0,0 +1,34 @@
+type Job = {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+};
+
+const exampleJob: Job = {
+  id: 1,
+  title: "Frontend Developer",
+  company: "Bearmentor",
+  location: "Remote",
+};
+
+const exampleJobs: Job[] = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    company: "Bearmentor",
+    location: "Remote",
+  },
+  {
+    id: 2,
+    title: "Backend Developer",
+    company: "Bearmentor",
+    location: "Remote",
+  },
+  {
+    id: 3,
+    title: "Full Stack Developer",
+    company: "Bearmentor",
+    location: "Remote",
+  },
+];
