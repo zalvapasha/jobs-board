c2612d47340eae2de8a1af479d7c4396c47dbab4
zalvapasha <zalvapasha1@gmail.com>
2025-02-01 21:39:14 +0700

    feat: get data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+export default async function Home() {
+  const resp = await fetch(`${process.env.API_URL}/jobs`);
+  const datas: Job[] = await resp.json();
+
+  console.log(datas);
+
   return (
     <div>
-      <h1>test</h1>
+      <h1>JOBS BOARD</h1>
+      {datas.map((job) => (
+        <li>{job.title}</li>
+      ))}
     </div>
   );
 }
